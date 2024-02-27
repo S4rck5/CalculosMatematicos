939d5b366e1b22bfa4c7086513be4ca3f2feec1b
S4rck5 <kevinsauced17@gmail.com>
2024-02-27 15:49:42 -0600

    cambio del programa

--- CalculosMatematicos/Program.cs
+++ CalculosMatematicos/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool salir = false;
+            int num1 = 0, num2 = 0;
+            while (!salir)
+            {
+                Console.WriteLine("1. Suma");
+                Console.WriteLine("2. Resta");
+                //Console.WriteLine("3. Multiplicacion");
+                //Console.WriteLine("4. Division");
+                Console.WriteLine("3. Salir");
+                int op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Proporciona el numero 1:");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Proporciona el numero 2:");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"el resultado de la suma entre {num1} y {num2} es: {calculos.Suma(num1, num2)}");
+
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Proporciona el numero 1:");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Proporciona el numero 2:");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"el resultado de la resta entre {num1} y {num2}es: {calculos.Resta(num1, num2)}");
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Gracias por usar el programa");
+                        salir = true;
+                        break;
+                }
+            }
         }
     }
-}
+}//agregar dividir y multiplicar y validaciones las mas comunes (validacion de que si se ingresa un string se pueda convertir en numero o no)
---
+++ CalculosMatematicos/calculos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculosMatematicos
+{
+    internal class calculos
+    {
+
+        public static int Suma(int num1,int num2){
+            return num1 + num2;
+        }
+        public static int Resta(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
