a1cb4ea64f354419fb16489722fb8a5e1a34bb6a
S4rck5 <kevinsauced17@gmail.com>
2024-02-27 15:02:33 -0600

    Creacion del proyecto

---
+++ CalculosMatematicos/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculosMatematicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
